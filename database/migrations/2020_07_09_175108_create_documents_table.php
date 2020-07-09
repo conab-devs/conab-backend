b64b99274448fa008d18082694c20306cdc69809
franklynkleiton <franklynkleiton18@gmail.com>
2020-07-09 18:35:13 -0300

    :card_file_box: Adding Document Migration

---
+++ database/migrations/2020_07_09_175108_create_documents_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable(false);
+            $table->string('path')->unique()->nullable(false);
+            $table->foreignId('cooperative_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+}
