1430a7ddb0b3e26cc6cbae853875054966c9bcd6
franklynkleiton <franklynkleiton18@gmail.com>
2020-07-09 18:36:39 -0300

    :card_file_box: Adding Cooperative Migration

---
+++ database/migrations/2020_07_09_174026_create_cooperatives_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCooperativesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cooperatives', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable(false);
+            $table->string('dap')->unique()->nullable(false);
+            $table->foreignId('addresses_id')
+                ->constrained()
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cooperatives');
+    }
+}
