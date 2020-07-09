6f429f2e1b681a8c8b2644bb09ed9f66093c9ce4
franklynkleiton <franklynkleiton18@gmail.com>
2020-07-09 18:21:22 -0300

    :card_file_box: Adding ProductCart Migration

---
+++ database/migrations/2020_07_09_211232_create_product_carts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_carts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('amount')->default(0);
+            $table->double('price')->nullable(false);
+            $table->dateTime('delivered_at')->nullable();
+            $table->foreignId('product_id')->constrained();
+            $table->foreignId('cart_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_carts');
+    }
+}
