c7aaac982046a344877e9238ae877ee739d96597
franklyn <franklynkleiton18@gmail.com>
2020-08-24 08:08:32 -0300

    refactor: change InvalidFieldException message

--- app/Components/Errors/InvalidFieldException.php
+++ app/Components/Errors/InvalidFieldException.php
@@ -6,7 +6,7 @@ class InvalidFieldException extends \Exception
 {
     public function __construct($message = '', $code = 0, \Exception $previous = null)
     {
-        parent::__construct("Invalid field: $message", $code, $previous);
+        parent::__construct('Invalid field', $code, $previous);
     }
 
     public function __toString()
