425dc29cb4d69edbd5f99fa929d230a5b4829148
franklyn <franklynkleiton18@gmail.com>
2020-08-25 10:46:01 -0300

    feat: ensure response success if user make login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+            'device_name' => 'required',
+        ]);
+
+        $user = User::where('email',$request->email)->first();
+
+        if (! $user) {
+            return response()->json(404, [
+                'message' => 'Theres no user with that email'
+            ]);
+        }
+        
+        $token = $user->login($request->password, $request->device_name)->plainTextToken;
+
+        return response()->json(['token' => $token]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->get('/hello', function (Request $request) {
+    return 'hello';
 });
+
+Route::post('/login', 'AuthController@login');
