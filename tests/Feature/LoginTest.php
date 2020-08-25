2fcfb41bf719379b63492dc4975e70990b8c84ee
franklyn <franklynkleiton18@gmail.com>
2020-08-25 10:46:18 -0300

    test: ensure response success if user make login

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+
+class LoginTest extends TestCase
+{
+    use WithFaker, RefreshDatabase;
+
+    /** @test */
+    public function should_make_login_and_return_token()
+    {
+        $faker = $this->faker();
+
+        $userCredentials = [
+            'email' => $faker->unique()->safeEmail,
+            'password' => 'valid_password'
+        ];
+        
+        factory(User::class)->create($userCredentials);
+
+        $userCredentials['device_name'] = 'MOBILE';
+
+        $response = $this->postJson('/api/login', $userCredentials);
+        
+        $response->assertStatus(200);
+        $this->assertArrayHasKey('token', $response);
+    }
+}
