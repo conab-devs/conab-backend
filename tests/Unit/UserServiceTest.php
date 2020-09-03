8e3978c39ea188543543e0c7f4f7cd53e5d65978
franklyn <franklynkleiton18@gmail.com>
2020-09-03 10:28:02 -0300

    test: ensure UserService findByEmail method return User

--- tests/Unit/UserServiceTest.php
+++ tests/Unit/UserServiceTest.php
@@ -2,12 +2,12 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
-use \Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
-use \Mockery;
 use App\Components\Errors\UnprocessableEntityException;
 use App\Components\Services\UserService;
 use App\User;
+use PHPUnit\Framework\TestCase;
+use \Mockery;
+use \Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 
 /** @group Franklyn */
 class UserServiceTest extends TestCase
@@ -21,8 +21,23 @@ class UserServiceTest extends TestCase
 
         $model = Mockery::mock(User::class);
         $model->shouldReceive('where->first')->andReturn(null);
-        
+
         $service = new UserService($model);
         $service->findByEmail('valid_email@mail.com');
+    }
+
+    /** @test */
+    public function user_service_find_by_email_should_return_user()
+    {
+        $user = new User;
+        $user->email = 'valid_email@mail.com';
+
+        $model = Mockery::mock(User::class);
+        $model->shouldReceive('where->first')->andReturn($user);
+
+        $service = new UserService($model);
+        $foundUser = $service->findByEmail($user->email);
+
+        $this->assertEquals($user->email, $foundUser->email);
     }
 }
