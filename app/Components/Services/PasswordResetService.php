c3c068e5b04f0733a10fe8f3bbf2842941683efa
franklyn <franklynkleiton18@gmail.com>
2020-09-03 12:30:28 -0300

    feat: ensure PasswordResetService store password reset request

--- app/Components/Services/PasswordResetService.php
+++ app/Components/Services/PasswordResetService.php
@@ -3,14 +3,16 @@
 namespace App\Components\Services;
 
 use App\PasswordReset;
+use App\Components\TokenGenerator;
 
 class PasswordResetService
 {
     private $model;
 
-    public function __construct(PasswordReset $reset)
+    public function __construct(PasswordReset $reset, TokenGenerator $tokenGenerator)
     {
         $this->model = $reset;
+        $this->generator = $tokenGenerator;
     }
 
     public function queryByEmail(string $email)
@@ -21,5 +23,13 @@ class PasswordResetService
     public function findByEmail(string $email)
     {
         return $this->queryByEmail($email)->first();
+    }
+
+    public function storePasswordResetRequest(string $email)
+    {
+        $token = $this->generator->generate();
+
+        $this->model->fill(['email' => $email, 'token' => $token]);
+        return $this->model->save();
     }
 }
