fbedb6140a02f2cb9a9c8f79d6c5b8c218657fe4
franklyn <franklynkleiton18@gmail.com>
2020-09-03 10:44:53 -0300

    feat: ensure PasswordResetService queryByEmail return query builder

---
+++ app/Components/Services/PasswordResetService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Components\Services;
+
+use App\PasswordReset;
+
+class PasswordResetService
+{
+    private $model;
+
+    public function __construct(PasswordReset $reset)
+    {
+        $this->model = $reset;
+    }
+
+    public function queryByEmail(string $email)
+    {
+        return $this->model->where('email', $email);
+    }
+}
