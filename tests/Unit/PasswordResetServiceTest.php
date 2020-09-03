fbf9d3a7160e365cb7fb082c667481377ef97aeb
franklyn <franklynkleiton18@gmail.com>
2020-09-03 11:45:24 -0300

    test: ensure password reset request is found

--- tests/Unit/PasswordResetServiceTest.php
+++ tests/Unit/PasswordResetServiceTest.php
@@ -25,4 +25,34 @@ class PasswordResetServiceTest extends TestCase
         
         $this->assertGreaterThan(0, $queryBuilder->count());
     }
+
+    /** @test */
+    public function password_reset_service_find_by_email_should_return_null()
+    {
+        $model = Mockery::mock(PasswordReset::class);
+        $model->shouldReceive('where->first')->andReturn(null);
+        
+        $service = new PasswordResetService($model);
+        $passwordReset = $service->findByEmail('invalid_email');
+        
+        $this->assertEquals(null, $passwordReset);
+    }
+
+    /** @test */
+    public function password_reset_service_find_by_email_should_return_password_reset()
+    {
+        $reset = new PasswordReset();
+        $reset->token = 'valid_token';
+        $reset->email = 'valid_mail@mail.com';
+
+        $model = Mockery::mock(PasswordReset::class);
+        $model->shouldReceive('where->first')->andReturn($reset);
+        
+        $service = new PasswordResetService($model);
+        $passwordReset = $service->findByEmail($reset->email);
+        
+        $this->assertEquals($reset->email, $passwordReset->email);
+        $this->assertEquals($reset->token, $passwordReset->token);
+
+    }
 }
