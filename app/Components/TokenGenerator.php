f9ab4d31b4662714f8e0ee3411aaa56d8b54948a
franklyn <franklynkleiton18@gmail.com>
2020-09-04 11:33:07 -0300

    fix: fix auth unit tests

--- app/Components/TokenGenerator.php
+++ app/Components/TokenGenerator.php
@@ -3,11 +3,17 @@
 namespace App\Components;
 
 use Illuminate\Support\Str;
+use Illuminate\Support\Facade\Auth;
 
 class TokenGenerator
 {
     public function generate()
     {
         return Str::random(80);
+    }
+
+    public function generateJwt($credentials)
+    {
+        Auth::attempt($credentials);
     }
 }
