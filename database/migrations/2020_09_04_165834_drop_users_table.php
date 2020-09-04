975c0a3caa317bacee72391a50ae6297d0e03d77
franklyn <franklynkleiton18@gmail.com>
2020-09-04 17:14:22 -0300

    fix: add tables creation

--- database/migrations/2020_09_04_165834_drop_users_table.php
+++ database/migrations/2020_09_04_165834_drop_users_table.php
@@ -73,17 +73,55 @@ class dropUsersTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('user_phones');
+        Schema::dropIfExists('user_addresses');
+        Schema::dropIfExists('product_carts');
+        Schema::dropIfExists('carts');
         Schema::dropIfExists('users');
 
-        Schema::table('users', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('profile_picture');
+            $table->string('profile_picture')->nullable();
             $table->string('email')->unique();
             $table->string('password');
             $table->string('cpf')->unique();
             $table->enum('user_type', ['CUSTOMER', 'ADMIN_CONAB', 'ADMIN_COOP'])->default('CUSTOMER');
             $table->foreignId('cooperative_id')->nullable()->constrained()->onDelete('CASCADE');
+            $table->timestamps();
+        });
+
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('total_price', 8, 2)->default(0.0);
+            $table->dateTime('closed_at')->nullable();
+            $table->boolean('is_closed')->default(false);
+            $table->foreignId('user_id')->constrained()->onDelete('CASCADE');
+            $table->timestamps();
+        });
+
+        Schema::create('product_carts', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('amount', 8, 2)->default(0.0);
+            $table->decimal('price', 8, 2);
+            $table->dateTime('delivered_at');
+            $table->enum('unit_of_measure', ['kg', 'unit']);
+            $table->foreignId('product_id')->constrained()->onDelete('CASCADE');
+            $table->foreignId('cart_id')->constrained()->onDelete('CASCADE');
+            $table->timestamps();
+        });
+
+        Schema::create('user_addresses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('CASCADE');
+            $table->foreignId('address_id')->constrained()->onDelete('CASCADE');
+            $table->timestamps();
+        });
+
+        Schema::create('user_phones', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('CASCADE');
+            $table->foreignId('phone_id')->constrained()->onDelete('CASCADE');
             $table->timestamps();
         });
     }
