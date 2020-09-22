2402de0af13a9eb6a1723ef0a0733eec349a565c
franklyn <franklynkleiton18@gmail.com>
2020-09-22 14:29:18 -0300

    feat: ensure full url is returned

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,13 +19,13 @@ use Faker\Provider\pt_BR\Person;
 
 $factory->define(User::class, function (Faker $faker) {
     $roles = ['CUSTOMER', 'ADMIN_COOP'];
-    
+
     $faker->addProvider(new Person($faker));
 
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'profile_picture' => $faker->file(base_path('tmp')),
+        'profile_picture' => 'uploads/kgmxqzFk0jaZyySVIKyb1piZZDxlhsTKIpSh6MBt.png',
         'password' => $faker->password(),
         'cpf' => $faker->cpf,
         'user_type' => $roles[rand(0, 1)]
