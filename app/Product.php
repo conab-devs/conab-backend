14d05ce801ca05ac71e913474eb9accc09110892
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-10-21 10:08:51 -0300

    feat: relationship with cooperative and category

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'price',
+        'photo_path',
+        'estimated_delivery_time',
+        'category_id',
+        'cooperative_id'
+    ];
+
+    protected $hidden = ['id', 'created_at', 'updated_at'];
+
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
+
+    public function cooperative()
+    {
+        return $this->belongsTo('App\Cooperative');
+    }
 }
