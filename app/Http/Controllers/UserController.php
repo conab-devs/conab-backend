5dd89458dcedcaa0d32d9874cdd2457bc373f73d
Franklyn Medeiros <franklynkleiton18@gmail.com>
2020-10-21 11:57:29 -0300

    feat: ensure that street is passed and that it's a string

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,7 +16,8 @@ class UserController extends Controller
             'cpf' => 'required|regex:/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}/|unique:users,cpf',
             'phones' => 'required|array',
             'phones.*.number' => 'required|string|regex:/^\([0-9]{2}\) [0-9]{5}\-[0-9]{4}/|distinct|unique:phones,number',
-            'addresses' => 'required'
+            'addresses' => 'required|array',
+            'addresses.*.street' => 'required|string'
         ]);
     }
 
