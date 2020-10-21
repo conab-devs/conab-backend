6a5713a95d837a4ce640d144a66439f051271032
Franklyn Medeiros <franklynkleiton18@gmail.com>
2020-10-21 14:40:05 -0300

    refactor: Adding transaction. Moving request validation to request form.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,33 +2,33 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\UserStore;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
 
 class UserController extends Controller
 {
-    public function store(Request $request)
+    public function store(UserStore $request)
     {
-        $validated = $request->validate([
-            'name' => 'required|string',
-            'email' => 'required|email|unique:users',
-            'password' => 'required|string|min:6',
-            'cpf' => 'required|regex:/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}/|unique:users,cpf',
-            'phones' => 'required|array',
-            'phones.*.number' => 'required|string|regex:/^\([0-9]{2}\) [0-9]{5}\-[0-9]{4}/|distinct|unique:phones,number',
-            'addresses' => 'required|array',
-            'addresses.*.street' => 'required|string',
-            'addresses.*.neighborhood' => 'required|string',
-            'addresses.*.city' => 'required|string',
-            'addresses.*.number' => 'required|string'
-        ]);
+        DB::beginTransaction();
 
-        $user = \App\User::create($validated);
-        $user->phones()->createMany($validated['phones']);
-        $user->addresses()->createMany($validated['addresses']);
-        $user->load('phones', 'addresses');
+        try {
+            $validated = $request->validated();
+            $user = \App\User::create($validated);
+            $user->phones()->createMany($validated['phones']);
+            $user->addresses()->createMany($validated['addresses']);
+            $user->load('phones', 'addresses');
 
-        return response()->json($user, 201);
+            DB::commit();
+
+            return response()->json($user, 201);
+        } catch (\Exception $error) {
+            DB::rollBack();
+
+            return response()->json([
+                "message" => "Algo deu errado, tente novamente em alguns instantes",
+            ], 500);
+        }
     }
 
     public function destroy(\App\User $user)
