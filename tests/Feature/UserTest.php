f44079d75950f2630d8971b2dc925a5b6c6ba93a
Franklyn Medeiros <franklynkleiton18@gmail.com>
2020-10-21 11:02:50 -0300

    feat: ensure that validation error is returned if existing email is passed

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -8,6 +8,8 @@ use Tests\TestCase;
 
 class UserTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
     public function should_return_validation_error_if_no_name_is_passed()
     {
@@ -90,6 +92,34 @@ class UserTest extends TestCase
         $response = $this->postJson('api/users', [
             'name' => 'valid_name',
             'email' => 'invalid_email',
+            'password' => 'valid_password',
+            'cpf' => 'valid_cpf',
+            'phones' => [
+                ['number' => '(00) 00000-0000'],
+                ['number' => '(11) 11111-1111'],
+            ],
+            'addresses' => [
+                ['street' => 'valid_street',
+                 'neighborhood' => 'valid_neighborhood',
+                 'city' => 'valid_city',
+                 'number' => 'any_num'],
+                ['street' => 'another_street',
+                 'neighborhood' => 'another_neighborhood',
+                 'city' => 'another_city',
+                 'number' => 'any_num'],
+            ],
+        ]);
+        $response->assertStatus(422);
+    }
+
+    /** @test */
+    public function should_return_validation_error_if_email_already_exists()
+    {
+        factory(\App\User::class)->create(['email' => 'valid_mail@mail.com']);
+
+        $response = $this->postJson('api/users', [
+            'name' => 'valid_name',
+            'email' => 'valid_mail@mail.com',
             'password' => 'valid_password',
             'cpf' => 'valid_cpf',
             'phones' => [
