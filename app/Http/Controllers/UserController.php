e4c298fbc128b7418bc7569ec9aa173682eef2cb
Franklyn Medeiros <franklynkleiton18@gmail.com>
2020-10-21 10:58:15 -0300

    feat: ensure that validation error is returned if no email is passed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,8 @@ class UserController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => 'required|string'
+            'name' => 'required|string',
+            'email' => 'required'
         ]);
     }
 
--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -58,4 +58,29 @@ class UserTest extends TestCase
         ]);
         $response->assertStatus(422);
     }
+
+    /** @test */
+    public function should_return_validation_error_if_no_email_is_passed()
+    {
+        $response = $this->postJson('api/users', [
+            'name' => 'valid_name',
+            'password' => 'valid_password',
+            'cpf' => 'valid_cpf',
+            'phones' => [
+                ['number' => '(00) 00000-0000'],
+                ['number' => '(11) 11111-1111'],
+            ],
+            'addresses' => [
+                ['street' => 'valid_street',
+                 'neighborhood' => 'valid_neighborhood',
+                 'city' => 'valid_city',
+                 'number' => 'any_num'],
+                ['street' => 'another_street',
+                 'neighborhood' => 'another_neighborhood',
+                 'city' => 'another_city',
+                 'number' => 'any_num'],
+            ],
+        ]);
+        $response->assertStatus(422);
+    }
 }
