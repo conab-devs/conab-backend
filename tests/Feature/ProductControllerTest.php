9c9b8991625e28833ce972d22280bcd491c23b95
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-10-26 09:36:23 -0300

    :white_check_mark: Add test create product

---
+++ tests/Feature/ProductControllerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Category;
+use App\Cooperative;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ProductControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function should_create_an_product()
+    {
+        $cooperative = factory(Cooperative::class)->create();
+        $admin_coop = factory(User::class)->create([
+            'cooperative_id' => $cooperative->id,
+            'user_type' => 'ADMIN_COOP'
+        ]);
+        $category = factory(Category::class)->create();
+
+        $data = [
+            'name' => 'any_name',
+            'price' => 9.99,
+            'photo_path' => 'path_file.png',
+            'estimated_delivery_time' => 1,
+            'cooperative_id' => $cooperative->id,
+            'category_id' => $category->id
+        ];
+
+        $response = $this->actingAs($admin_coop, 'api')
+            ->postJson('/api/products', $data);
+
+        $response->assertStatus(201)
+            ->assertJsonFragment($data);
+    }
+}
