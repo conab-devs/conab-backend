02f313279b7100bfa5f2e69291379a75845ee2e0
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-10-26 11:24:03 -0300

    :sparkles: Create product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 
 class ProductController extends Controller
 {
@@ -20,7 +21,26 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $user = $request->user();
+
+        $request->validate([
+            'name' => 'bail|required|max:255',
+            'price' => 'required|numeric|between:0,99999999.99',
+            'photo_path' => 'required|image',
+            'estimated_delivery_time' => 'required|integer',
+            'category_id' => 'required|exists:App\Category,id',
+        ]);
+
+        $product = new Product();
+        $product->fill($request->all());
+        $product->cooperative_id = $user->cooperative_id;
+        $product->photo_path = App::environment('production')
+            ? $this->uploadFileOnFirebase($request->file('photo_path'))
+            : $request->file('photo_path')->store('uploads');
+
+        $product->save();
+
+        return response($product, 201);
     }
 
     /**
