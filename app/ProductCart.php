280f73f8ad216bc5ca28933f9830e462fe246f71
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-01-13 10:57:47 -0300

    :wrench: Adicionando atributos preenchíveis

--- app/ProductCart.php
+++ app/ProductCart.php
@@ -8,9 +8,11 @@ class ProductCart extends Model
 {
     protected $fillable = [
         'product_id',
+        'cart_id',
         'price',
         'amount',
         'unit_of_measure',
+        'delivered_at'
     ];
 
     protected $hidden = ['created_at', 'updated_at'];
