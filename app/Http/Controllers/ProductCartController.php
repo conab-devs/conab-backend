7cd854dc9b829cf926ab458c52df714504eccbf7
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-01-14 19:12:43 -0300

    :sparkles: Atualizando quantidade do produto no carrinho

--- app/Http/Controllers/ProductCartController.php
+++ app/Http/Controllers/ProductCartController.php
@@ -4,29 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Cart;
 use App\Http\Requests\ProductCart\StoreRequest;
+use App\Http\Requests\ProductCart\UpdateRequest;
 use App\Product;
 use App\ProductCart;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class ProductCartController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(StoreRequest $request)
     {
         $id = auth()->user()->id;
@@ -45,7 +34,7 @@ class ProductCartController extends Controller
                 'cart_id' => $cart->id,
             ]);
 
-            $product_cart->fill(array_merge($request->all(), [
+            $product_cart->fill(array_merge($request->except('cart_id'), [
                 'unit_of_measure' => $product->unit_of_measure,
                 'price' => $product->price,
             ]));
@@ -62,35 +51,20 @@ class ProductCartController extends Controller
         }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+
+    public function update(UpdateRequest $request, ProductCart $productCart)
     {
-        //
+        $productCart->amount = $request->input('amount');
+        $productCart->save();
+
+        return response()->json($productCart, 200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //
---
+++ app/Http/Requests/ProductCart/UpdateRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests\ProductCart;
+
+use App\Http\Requests\FormRequestTrait;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
+
+class UpdateRequest extends FormRequest
+{
+    use FormRequestTrait;
+
+    public function authorize()
+    {
+        return Gate::allows('manage-product-cart', $this->route('productCart'));
+    }
+
+    public function rules()
+    {
+        return [
+            'amount' => 'required|numeric|between:0,99999999.99',
+        ];
+    }
+}
