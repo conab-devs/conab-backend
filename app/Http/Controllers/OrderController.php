a51c46506748c4293f8c049d7c6975389b24df07
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-02-01 15:05:36 -0300

    :memo: Adicionando documentação do OrderController

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -7,9 +7,33 @@ use App\Cart;
 class OrderController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * @OA\Get(
+     *     path="/order",
+     *     operationId="index",
+     *     summary="Retorna lista de pedido do usuário logado",
+     *     tags={"Pedidos"},
      *
-     * @return \Illuminate\Http\Response
+     *     @OA\Response(
+     *         response=200,
+     *         description="OK",
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 allOf={
+     *                     @OA\Schema(ref="#/components/schemas/Order"),
+     *                     @OA\Schema(
+     *                         @OA\Property(
+     *                             property="carts",
+     *                             type="array",
+     *                             @OA\Items(ref="#/components/schemas/Cart")
+     *                         )
+     *                     )
+     *                 }
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(response=500, description="Server Error")
+     * )
      */
     public function index()
     {
@@ -23,13 +47,47 @@ class OrderController extends Controller
     }
 
     /**
-     * Display the specified resource.
+     * @OA\Get(
+     *     path="/order/{id}",
+     *     operationId="show",
+     *     summary="Retorna um pedido pelo ID",
+     *     tags={"Pedidos"},
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     *     @OA\Parameter(
+     *         name="id",
+     *         description="Id do Pedido",
+     *         required=true,
+     *         in="path",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *
+     *     @OA\Response(
+     *         response=200,
+     *         description="OK",
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 allOf={
+     *                     @OA\Schema(ref="#/components/schemas/Order"),
+     *                     @OA\Schema(
+     *                         @OA\Property(
+     *                             property="carts",
+     *                             type="array",
+     *                             @OA\Items(ref="#/components/schemas/Cart")
+     *                         )
+     *                     )
+     *                 }
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(response=200, description="Ok"),
+     *     @OA\Response(response=404, description="Not Found"),
+     *     @OA\Response(response=500, description="Server Error")
+     * )
      */
     public function show($id)
     {
+
         $user = auth()->user();
 
         $order = $user->orders()
@@ -41,10 +99,25 @@ class OrderController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
+     * @OA\Delete(
+     *     path="/order/{id}",
+     *     operationId="destroy",
+     *     summary="Exclui o pedido",
+     *     tags={"Pedidos"},
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     *     @OA\Parameter(
+     *         name="id",
+     *         description="Id do pedido",
+     *         required=true,
+     *         in="path",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *
+     *     @OA\Response(response=204, description="No Content"),
+     *     @OA\Response(response=400, description="Bad Request"),
+     *     @OA\Response(response=404, description="Not Found"),
+     *     @OA\Response(response=500, description="Server Error")
+     * )
      */
     public function destroy($id)
     {
