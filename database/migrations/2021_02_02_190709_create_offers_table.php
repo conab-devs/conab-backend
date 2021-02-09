230dd86e701503096e22fd4721c6ef5bc1318179
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-02-09 09:31:03 -0300

    :card_file_box: Deletando tabela de conversa e adicionado tabela de oferta

--- database/migrations/2021_02_02_190709_create_offers_table.php
+++ database/migrations/2021_02_02_190709_create_offers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateConversationsTable extends Migration
+class CreateOffersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,10 @@ class CreateConversationsTable extends Migration
      */
     public function up()
     {
-        Schema::create('conversations', function (Blueprint $table) {
+        Schema::create('offers', function (Blueprint $table) {
             $table->id();
+            $table->dateTime('expired_at');
+            $table->decimal('discount', 8, 2);
             $table->foreignId('cart_id');
             $table->timestamps();
         });
