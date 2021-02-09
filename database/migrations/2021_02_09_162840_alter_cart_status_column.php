cd3247e9082179da87793ea81f69b328b5cf7805
franklynkleiton <franklynkleiton18@gmail.com>
2021-02-09 13:36:57 -0300

    Fixing cart status column.

---
+++ database/migrations/2021_02_09_162840_alter_cart_status_column.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterCartStatusColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::table('carts', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+
+        Schema::table('carts', function (Blueprint $table) {
+            $table->enum('status', ['Aberto', 'Aguardando Pagamento', 'Concluído']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('carts', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+
+        Schema::table('carts', function (Blueprint $table) {
+            $table->string('status');
+        });
+    }
+}
